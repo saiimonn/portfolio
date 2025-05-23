2c39c4c8b48be55014f907e8e218560394ccd7cc
Sai <gementizasgg08@gmail.com>
2025-05-23 21:22:02 +0800

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import '../styles/index.css'
+import { motion } from "framer-motion"
+
+function App() {
+
+  return (
+    <>
+      <main className = "w-full h-screen dark:bg-black bg-white dark:text-white text-black">
+        
+      </main>
+    </>
+  )
+}
+
+export default App
